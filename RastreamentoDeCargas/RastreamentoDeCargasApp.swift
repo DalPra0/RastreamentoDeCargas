148cbe230a8f77a0c41c77f47835f7e3635c19ae
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-08-15 11:02:26 -0300

    Initial Commit

---
+++ RastreamentoDeCargas/RastreamentoDeCargasApp.swift
@@ -0,0 +1,17 @@
+//
+//  RastreamentoDeCargasApp.swift
+//  RastreamentoDeCargas
+//
+//  Created by Lucas Dal Pra Brascher on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RastreamentoDeCargasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
